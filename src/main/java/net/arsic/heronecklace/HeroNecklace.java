0655506e8520bf0f663d4e4b5ca79e9d4aa37c9d
Harrison Craig <harrison.craig.20@cnu.edu>
2024-09-24 20:00:44 -0400

    Initial commit

---
+++ src/main/java/net/arsic/heronecklace/HeroNecklace.java
@@ -0,0 +1,18 @@
+package net.arsic.heronecklace;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HeroNecklace implements ModInitializer {
+	public static final String MOD_ID = "heronecklace";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/arsic/heronecklace/HeroNecklaceClient.java
@@ -0,0 +1,10 @@
+package net.arsic.heronecklace;
+import net.fabricmc.api.ClientModInitializer;
+
+
+public class HeroNecklaceClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
